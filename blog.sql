99ba42b7e79b20a22444e385fb3b4d3069478330
aerisadm <81646062+aerisadm@users.noreply.github.com>
2022-09-27 20:07:47 -0300

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 28-Set-2022 às 01:06
--- Versão do servidor: 10.4.18-MariaDB
--- versão do PHP: 8.0.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Banco de dados: `blog`
